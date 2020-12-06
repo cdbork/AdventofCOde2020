a3816455f2ec33f1a57edc78a152ab67020b4f34
cdbork <cdbork@gmail.com>
2020-12-06 11:27:10 -0600

    Day6

---
+++ Day6/Day6_Part1.ps1
@@ -0,0 +1,18 @@
+﻿$AllInput=Get-Content C:\GitHub\AOC2020\Day6\Day6_Input.txt
+#$AllInput=Get-Content C:\GitHub\AOC2020\Day6\Day6_Test.txt
+
+$CurrentGroup
+$GroupYesTotal=0
+
+Foreach ($Line in $AllInput) {
+if ($Line -ne "") {
+    $CurrentGroup=$CurrentGroup+$Line
+}
+else {
+    $CurrentGroup
+    $charArray=$currentGroup.ToCharArray()
+    $GroupYesTotal+=(($CharArray | group) | Measure-Object).count
+    $CurrentGroup=""
+}
+}
+$GroupYesTotal
---
+++ Day6/Day6_Part2.ps1
@@ -0,0 +1,27 @@
+﻿$AllInput=Get-Content C:\GitHub\AOC2020\Day6\Day6_Input.txt
+#$AllInput=Get-Content C:\GitHub\AOC2020\Day6\Day6_Test.txt
+
+$CurrentGroup
+$CurrentGroupCount=0
+$GroupYesTotal=0
+
+Foreach ($Line in $AllInput) {
+if ($Line -ne "") {
+    $CurrentGroup=$CurrentGroup+$Line
+    $CurrentGroupCount++
+}
+else {
+    #$CurrentGroup
+    $charArray=$currentGroup.ToCharArray()
+    #Write-Host "Group Size: $CurrentGroupCount"
+    $GroupedChar=($CharArray | group)
+    Foreach($thing in $GroupedChar)
+    {
+        if($thing.Count -eq $CurrentGroupCount) { $GroupYesTotal++ }
+    }
+    #$GroupYesTotal+=(($CharArray | group) | Measure-Object)
+    $CurrentGroup=""
+    $CurrentGroupCount=0
+}
+}
+$GroupYesTotal
